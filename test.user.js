d3b5a901e11dca56585c30d8ebbdade9cbefa9ad
SmallGlass <xinchaozhulang@126.com>
2016-10-27 10:42:11 +0800

    first commit

---
+++ test.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        mooncake
+// @namespace   com.mzwu
+// @description mooncake
+// @include     http://localhost:8080/test.html
+// @version     1
+// @grant       none
+// ==/UserScript==
+ setInterval(function(){
+	$("#rush").trigger('click')
+},1)
